930507d097d58d3df8bac0d979bc70b27e38ac43
Jared Edwards <jared@kubefirst.com>
2024-02-27 23:43:37 -0700

    first attempt at outputs for hostfile

--- civo/examples/terraform/kubernetes-vms/outputs.tf
+++ civo/examples/terraform/kubernetes-vms/outputs.tf
@@ -1,7 +1,31 @@
-# output "kubernetes_vms_public_ips" {
-#   value = "--servers-public-ips ${module.k1_master_1.public_ip},${module.k1_master_2.public_ip},${module.k1_master_3.public_ip}"
-# }
+output "kubernetes_vms_public_ips" {
+  value = "--servers-public-ips ${module.k1_master_1.public_ip},${module.k1_master_2.public_ip},${module.k1_master_3.public_ip}"
+}
 
-# output "kubernetes_vms_private_ips" {
-#   value = "--servers-private-ips ${module.k1_master_1.private_ip},${module.k1_master_2.private_ip},${module.k1_master_3.private_ip}"
-# }
+output "kubernetes_vms_private_ips" {
+  value = "--servers-private-ips ${module.k1_master_1.private_ip},${module.k1_master_2.private_ip},${module.k1_master_3.private_ip}"
+}
+
+output "k1_master_1_public_hostfile_entry" {
+  value = module.k1_master_1.public_ip_hostname
+}
+
+output "k1_master_1_private_hostfile_entry" {
+  value = module.k1_master_1.private_ip_hostname
+}
+
+output "k1_master_2_public_hostfile_entry" {
+  value = module.k1_master_2.public_ip_hostname
+}
+
+output "k1_master_2_private_hostfile_entry" {
+  value = module.k1_master_2.private_ip_hostname
+}
+
+output "k1_master_3_public_hostfile_entry" {
+  value = module.k1_master_3.public_ip_hostname
+}
+
+output "k1_master_3_private_hostfile_entry" {
+  value = module.k1_master_3.private_ip_hostname
+}
--- civo/terraform/modules/kubernetes-vms/outputs.tf
+++ civo/terraform/modules/kubernetes-vms/outputs.tf
@@ -5,3 +5,11 @@ output "public_ip" {
 output "private_ip" {
   value = civo_instance.this.private_ip
 }
+
+output "public_ip_hostname" {
+  value = "${civo_instance.this.public_ip} ${var.name}"
+}
+
+output "private_ip_hostname" {
+  value = "${civo_instance.this.private_ip} ${var.name}"
+}
