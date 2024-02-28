d8669c0fd67d3aef88794b23542c504bb836fb46
Jared Edwards <jared@kubefirst.com>
2024-02-28 14:26:17 -0700

    update output name collision

--- civo/examples/terraform/kubernetes-vms/outputs.tf
+++ civo/examples/terraform/kubernetes-vms/outputs.tf
@@ -1,8 +1,8 @@
-output "kubernetes_vms_private_ips" {
+output "kubernetes_vms_private_ips_flags" {
   value = "--servers-private-ips ${module.k1_master_1.private_ip},${module.k1_master_2.private_ip},${module.k1_master_3.private_ip}"
 }
 
-output "kubernetes_vms_public_ips" {
+output "kubernetes_vms_public_ips_flags" {
   value = "--servers-public-ips ${module.k1_master_1.public_ip},${module.k1_master_2.public_ip},${module.k1_master_3.public_ip}"
 }
 
