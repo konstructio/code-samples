54d5a4f80667650dccc93dd50514e282b6839223
Jared Edwards <jared@kubefirst.com>
2024-02-27 20:07:57 -0700

    adjust outputs to acommodate private ip

--- civo/examples/terraform/kubernetes-vms/outputs.tf
+++ civo/examples/terraform/kubernetes-vms/outputs.tf
@@ -1,3 +1,7 @@
-output "kubernetes_vms_public_ips" {
-  value = [module.k1_master_1.public_ip, module.k1_master_2.public_ip, module.k1_master_3.public_ip]
-}
+# output "kubernetes_vms_public_ips" {
+#   value = "--servers-public-ips ${module.k1_master_1.public_ip},${module.k1_master_2.public_ip},${module.k1_master_3.public_ip}"
+# }
+
+# output "kubernetes_vms_private_ips" {
+#   value = "--servers-private-ips ${module.k1_master_1.private_ip},${module.k1_master_2.private_ip},${module.k1_master_3.private_ip}"
+# }
--- civo/terraform/modules/kubernetes-vms/outputs.tf
+++ civo/terraform/modules/kubernetes-vms/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = civo_instance.this.public_ip
 }
+
+output "private_ip" {
+  value = civo_instance.this.private_ip
+}
