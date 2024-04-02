8be979a18156b114e3d41ebce7cb9706eca95f77
Jared Edwards <jared@kubefirst.com>
2024-04-02 13:07:37 -0600

    fix ssh path var for update etc hosts

--- civo/examples/terraform/kubernetes-vms/variables.tf
+++ civo/examples/terraform/kubernetes-vms/variables.tf
@@ -10,7 +10,7 @@ variable "ssh_key_pub" {
   type = string
 }
 
-variable "ssh_key_private" {
-  description = "Private key for instance ssh"
+variable "ssh_key_private_path" {
+  description = "Private key path for configuring /etc/hosts/ for each instance"
   type        = string
 }
